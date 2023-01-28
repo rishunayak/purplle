b5ef86bfe46869dbc9356339725cf6656a2ac518
vivek13420 <vivekrana13420@gmail.com>
2023-01-28 21:15:22 +0530

    changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Cart from './Pages/Cart/Cart';
 import UserProfile from './Pages/UserProfile/UserProfile';
 
 function App() {
   return (
     <div className="App">
-      <UserProfile />
+      <Cart/>
     </div>
   );
 }
