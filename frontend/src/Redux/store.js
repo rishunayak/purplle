4198e7e38003d8442a4b71b8c6098e26bccff51f
rishunayak <rishunayak22@email.com>
2023-01-29 15:48:22 +0530

    Update Store for Route of Facewash

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -5,8 +5,9 @@ import thunk from "redux-thunk";
 import { AuthReducer } from "./User/reducer";
 import { ShampooReducer } from "./Shampoo/reducer";
 import { PrimerReducer } from "./Primer/reducer";
+import { FaceWashReducer } from "./FaceWash/reducer";
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const rootReducer = combineReducers({ AuthReducer,ShampooReducer,PrimerReducer});
+const rootReducer = combineReducers({ AuthReducer,ShampooReducer,PrimerReducer,FaceWashReducer});
 export const store = legacy_createStore( rootReducer,composeEnhancers(applyMiddleware(thunk)));
