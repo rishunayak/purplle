1f859fdd0502c87c3d2ee5b110b0b3f17a8783c6
rishunayak <rishunayak22@email.com>
2023-01-29 13:17:03 +0530

    Updated Routes for Shampoo

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -3,8 +3,9 @@
 import { legacy_createStore, applyMiddleware,compose,combineReducers,} from "redux";
 import thunk from "redux-thunk";
 import { AuthReducer } from "./User/reducer";
+import { ShampooReducer } from "./Shampoo/reducer";
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const rootReducer = combineReducers({ AuthReducer});
+const rootReducer = combineReducers({ AuthReducer,ShampooReducer});
 export const store = legacy_createStore( rootReducer,composeEnhancers(applyMiddleware(thunk)));
