d6af3f7dc9840c1b06c3d864df069eb1240b50df
Rsaikiran92 <saikiran92rk@gmail.com>
2023-01-30 16:36:40 +0530

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import Cart from './Pages/Cart/Cart';
 import Home from './Components/Home';
 import UserProfile from './Pages/UserProfile/UserProfile';
+import Navbar1 from './Components/Navbar1';
+import Navbar2 from './Components/Navbar2';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/*<Navbar1/>*/}
+      <Footer/>
+      {/*<Home/>*/}
       {/*<UserProfile />*/}
     </div>
   );
