ba7bda936f57fec5ad446b94ec2af4536813fe82
Rsaikiran92 <saikiran92rk@gmail.com>
2023-01-31 00:06:16 +0530

    sort function add to product page

--- frontend/src/Components/Box.jsx
+++ frontend/src/Components/Box.jsx
@@ -1,14 +1,36 @@
 import "./Box.css"
 
 function Box({item}){
-    console.log(item)
-    return(<div className="item">
-        <div><img width={"200px"} height={"200px"} src={item.image} /></div>
-        <div className="cartbutton"><button>ADD TO CART</button></div>
-        <div>fdhhhhhhhhhhhhhhhhhhhhhhhhhhhh</div>
-        <div>255</div>
+    
+    return(
+    <div className="item">
+
+        <div>
+            <img className="img" src={item.image} alt="image" />
+        </div>
+
+        <div className="cartbutton">
+            <button>ADD TO CART</button>
+        </div>
+
+        <div className="Productsname">{item.product_name}</div>
+
+        <div className="pricerating">
+            <div>
+                <b>₹ {item.d_price}</b>
+            </div>
+            <div className="price">{item.price}</div>
+            <div className="offer">{item.offer}</div>
+        </div>
+
+        <div className="pricerating">
+            <div className="rating">{item.rating}⭐️</div>
+            <div className="ratingcount">{item.rating_count}</div>
+        </div>
+        
         <div></div>
-    </div>)
+    </div>
+    )
 }
 
 export default Box
--- frontend/src/Components/Products.jsx
+++ frontend/src/Components/Products.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react"
-import { useEffect } from "react"
+import { useEffect , useState} from "react"
 import Box from "./Box"
 import "./Products.css"
 
@@ -7,12 +6,34 @@ import "./Products.css"
 
 function Products(){
     const [Data,setdata]=useState([])
+
+
     useEffect(()=>{
         fetch("https://purplebackend.onrender.com/products/shampoo").then((r)=>r.json()).then((r)=>setdata(r))
     },[])
-    return(<div className="ProductsBox" style={{paddingLeft:"10%",paddingRight:"10%",border:"2px solid red"}}> 
-        {Data.map((item)=><Box item={item}></Box>)}
-    </div>)
+
+
+    return(
+        <div>
+            <div className="menubox">
+            <select className='menu'>
+                <option value>Price Range</option>
+                <option value='res'>100 below</option>
+                <option value='asc'>Rs. 100 - Rs. 200</option>
+                <option value='desc'>Rs. 200 - Rs. 500</option>
+                <option value='desc'>Rs. 500 - Rs. 1000</option>
+            </select>
+            <select className='menu' style={{marginLeft:"10px"}}>
+                <option value>Sort By</option>
+                <option value='asc'>Ascending</option>
+                <option value='desc'>Descending</option>
+            </select>
+            </div>
+            <div className="ProductsBox"> 
+                {Data.map((item)=><Box item={item}></Box>)}
+            </div>
+        </div>
+    )
 }
 
 export default Products
