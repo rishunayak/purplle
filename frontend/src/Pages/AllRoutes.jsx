d451caa131dd5f05a0d8308955d6f2cbac30bf58
vivek13420 <vivekrana13420@gmail.com>
2023-01-31 12:18:47 +0530

    added productDetails page

--- frontend/src/Pages/AllRoutes.jsx
+++ frontend/src/Pages/AllRoutes.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Cart from './Cart/Cart'
 import Login from './Login/Login'
+import ProductDetails from './ProductDetails/ProductDetails'
 
 const AllRoutes = () => {
   return (
     <>
       <Routes>
+        <Route path='/prductDetails' element={<ProductDetails/>}/>
         <Route path='/' element={<Login/>}/>
         <Route path='/cart' element={<Cart/>}/>
       </Routes>
